5b9118bd0a979806cbaf8635797646746477bcf4
ZhaoWenZhuo <167313298+MineGuYan@users.noreply.github.com>
2025-07-11 15:36:16 +0800

    删除Vue初始化自带的无用文件

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 8080,
+    // host: "0.0.0.0",
+    host: "127.0.0.1",
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      }
+    }
+  }
 })
